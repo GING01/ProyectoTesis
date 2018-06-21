5e139cc2d5b787ae090f5718bd847ac13cf85e0f
GING01 <andres.paredes@epn.edu.ec>
2018-06-21 15:52:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/and_a/proyecto_tesis/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.and_a.proyecto_tesis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
